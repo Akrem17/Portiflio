6489aa8990e82c6d75289dc065c25f232c769f84
Akrem Hammami <akremhammami@outlook.com>
2022-04-28 12:44:32 +0100

    add small screen home

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,3 +1,4 @@
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body.component.scss']
 })
 export class BodyComponent implements OnInit {
+  displayOn:string="displayOn"
+  displayOff:string="displayOff"
+  
+  innerWidth: number | undefined;
 
-  constructor() { }
+  constructor(    private breakpointObserver: BreakpointObserver) { 
+      this.breakpointObserver.observe([
+        "(max-width: 768px)"
+      ]).subscribe((result: BreakpointState) => {
+        if (result.matches) {
+            
+                this.displayOff=""
+                this.displayOn="displayOff"
+               
+        } else {
+          this.displayOff="displayOff"
+          this.displayOn=""
 
+        }
+    })
+  }
   ngOnInit(): void {
   }
 
--- src/app/card-big-screen/card-big-screen.component.ts
+++ src/app/card-big-screen/card-big-screen.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-card-big-screen',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-big-screen.component.scss']
 })
 export class CardBigScreenComponent implements OnInit {
+  myClass:string ="center";
+  wrapper:string ="wrapper";
+  displayOn:string="displayOn"
+  displayOff:string="displayOff"
+  
+  innerWidth: number | undefined;
 
-  constructor() { }
+  constructor(    private breakpointObserver: BreakpointObserver) { 
+      this.breakpointObserver.observe([
+        "(max-width: 768px)"
+      ]).subscribe((result: BreakpointState) => {
+        if (result.matches) {
+            
+                this.displayOff=""
+                this.displayOn="displayOff"
+                console.log("hilo")
+               
+        } else {
+          this.displayOff="displayOff"
+          this.displayOn=""
 
-  ngOnInit(): void {
+          console.log("hvbbilo")
+        }
+    })
   }
 
+  ngOnInit(): void {
+      
+  }
 }
+
